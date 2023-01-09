0cd18c5b73f8ce331681adac343dc0d6e1134254
marquesaline <alinem_oliveira@yahoo.com>
2023-01-09 20:54:41 -0300

    função de criação da tarefa

--- src/app.py
+++ src/app.py
@@ -1,14 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from src.task_controller import TaskController
 
 
 app = Flask(__name__)
 
-@app.route('/tasks/', methods=['GET'])
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    try: 
+        task = request.get_json('task')
+
+        result = TaskController.create_task(task)
+        print(result)
+
+        return jsonify({
+            'status_code': 200,
+            'message': 'Tarefa criada com sucesso',
+            'task': result
+        }), 200
+    
+    except Exception as error:
+        return jsonify({
+            'status_code': 500,
+            'message': 'Erro ao cadastrar a tarefa',
+            'error': str(error)
+        }), 500
+
+
+@app.route('/tasks', methods=['GET'])
 def get_tasks():
     try: 
         tasks = TaskController.get_all_tasks()
-        print(tasks)
         return jsonify({
             'status_code': 200,
             'tasks': tasks
@@ -16,9 +38,9 @@ def get_tasks():
         
     except Exception as error:
         return jsonify({
-            'error': str(error),
+            'status_code': 500,
             'message': 'Erro ao buscar as tarefas',
-            'status_code': 500
+            'error': str(error)
         }), 500
 
 
--- src/task_controller.py
+++ src/task_controller.py
@@ -1,29 +1,67 @@
+import json
+
 from src.task_schema import TaskSchema
 from src.database import Database
 
 class TaskController:
 
+    def create_task(task: TaskSchema):
+        try:
+            db = Database.connect_db()
+            cursor = db.cursor()
+
+            sql_query = 'INSERT INTO tasks (title, description) VALUES(?, ?)'
+
+            title = task['title']
+            description = task['description']
+
+            cursor.execute(sql_query, [title, description])
+            db.commit()
+            
+            # retorna a tarefa criada
+            result = TaskController.get_task_by_id(cursor.lastrowid)
+           
+            db.close()
+
+            return result
+        except Exception as error:
+            return error
 
     def get_all_tasks():
-
         try:
-
             db = Database.connect_db()
             cursor = db.cursor()
             
             cursor.execute(f'SELECT * FROM tasks')
+            tasks = []
 
-            tasks = cursor.fetchall()
+            for task in cursor.fetchall():
+                tasks.append({
+                    'id':task[0],
+                    'title':task[1],
+                    'description':task[2]
+                })
 
-            print(tasks)
+            db.close()
 
-            result = []
-
-            for task in tasks:
-                result.append(task)
-
-            return result
-
+            return tasks
         except Exception as error:
             return error
+
+    def get_task_by_id(id: int):
+        try:
+            db = Database.connect_db()
+            cursor = db.cursor()
+            
+            cursor.execute(f'SELECT * FROM tasks WHERE id={int(id)}')
+            db.commit()
+            task = cursor.fetchone()
+          
+            db.close()
+
+            return task
+        except Exception as error:
+            return error
+
+
     
